614ce3ad18fa9d7edc8e1ff70066ad3888f1a941
polojerry <polojerry4@gmail.com>
2019-12-31 11:31:20 +0300

    Scheduled a noteUploader

--- app/src/main/java/com/jwhh/notekeeper/utils/NoteUploaderJobService.java
+++ app/src/main/java/com/jwhh/notekeeper/utils/NoteUploaderJobService.java
@@ -1,17 +1,40 @@
 package com.jwhh.notekeeper.utils;
 
+import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.net.Uri;
+import android.os.AsyncTask;
 
 public class NoteUploaderJobService extends JobService {
 
     public static String EXTRA_JOB_DATA = "com.jwhh.notekeeper.utils.extra.NOTE_URI";
+    private NoteUploader mNoteUploader;
+
     public NoteUploaderJobService() {
     }
 
     @Override
-    public boolean onStartJob(JobParameters params) {
-        return false;
+    public boolean onStartJob(final JobParameters params) {
+        @SuppressLint("StaticFieldLeak") AsyncTask<JobParameters, Void,Void> task = new AsyncTask<JobParameters, Void, Void>() {
+            @Override
+            protected Void doInBackground(JobParameters... jobParameters) {
+                JobParameters backgroundParameters = jobParameters[0];
+
+                String jobStringUri = backgroundParameters.getExtras().getString(EXTRA_JOB_DATA);
+                Uri jobUri = Uri.parse(jobStringUri);
+                mNoteUploader.doUpload(jobUri);
+
+
+                jobFinished(backgroundParameters,false);
+                return null;
+            }
+        };
+
+        mNoteUploader = new NoteUploader(this);
+        task.execute(params);
+
+        return true;
     }
 
     @Override
